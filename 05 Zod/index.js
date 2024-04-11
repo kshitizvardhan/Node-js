8184f9fce46a354683cb3ae92a82ac034fe63b47
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-04-11 21:38:38 +0530

    Using Zod for schema validation

---
+++ 05 Zod/index.js
@@ -0,0 +1,23 @@
+const zod = require('zod');
+
+function validateInput(obj){
+    const schema = zod.object({
+        email: zod.string().email(),
+        password: zod.string().min(6).max(12)       // minimum 6 and maximum 12 character password
+    })
+
+    const response = schema.safeParse(obj);
+    console.log(response);
+    if(response.success){
+        const email = response.data.email;
+        const password = response.data.password;
+        console.log(email);
+        console.log(password)
+    }
+
+}
+
+validateInput({
+    email: "kshitizvardhan8@gmail.com",
+    password: "fsdj33j45k4k"
+})
